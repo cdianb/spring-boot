c1177878510aea2e5f38ca7eeb6c2ca3a861b60f
meiquanliang <1532393623@qq.com>
2023-06-11 14:55:48 +0800

    @Configuration注解和@import注解

--- src/main/java/com/mei/SpringBoot01HelloWorldApplication.java
+++ src/main/java/com/mei/SpringBoot01HelloWorldApplication.java
@@ -1,15 +1,62 @@
 package com.mei;
 
+import ch.qos.logback.core.db.DBHelper;
+import com.mei.beans.Pet;
+import com.mei.beans.User;
+import com.mei.config.MyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
-//@SpringBootApplication 标注这个类是一个spring boot的应用,启动类下的所有资源被导入
+/**
+ * 主程序类。主配置类
+ * @SpringBootApplication 标注这个类是一个spring boot的应用,启动类下的所有资源被导入
+ *
+ */
 @SpringBootApplication(scanBasePackages = "com.mei")
 public class SpringBoot01HelloWorldApplication {
 
     public static void main(String[] args) {
-        //将springBoot应用启动
-        SpringApplication.run(SpringBoot01HelloWorldApplication.class, args);
+        //1.将springBoot应用启动,返回IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(SpringBoot01HelloWorldApplication.class, args);
+
+        //2.查看容器裏面的组件
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        //3.从容器中获取组件
+        Pet tom01 = run.getBean("tom", Pet.class);
+        Pet tom02 = run.getBean("tom", Pet.class);
+        System.out.println(tom02 == tom01);
+
+        //4.com.mei.config.MyConfig$$EnhancerBySpringCGLIB$$36be1327@1e469dfd
+        MyConfig bean = run.getBean(MyConfig.class);
+        System.out.println(bean);
+
+        //如果@Configuration(proxyBeanMethods = true) 代理对象调用方法。SpringBoot总会检查这组件是否在容器中，保持组件单实例
+        User user = bean.user01();
+        User user1 = bean.user01();
+        System.out.println(user1 == user);
+
+        User user01 = run.getBean("user01", User.class);
+        Pet tom = run.getBean("tom", Pet.class);
+
+        System.out.println("用户信息：" + user01.toString());
+        System.out.println("用户的宠物：" + user01.getPet());
+        System.out.println("容器内的宠物：" + tom);
+        System.out.println("用户的宠物是不是容器内的宠物：" + (user01.getPet() == tom));
+
+        //5.获取组件
+        String[] namesForType = run.getBeanNamesForType(User.class);
+        System.out.println("======================================");
+        for (String s : namesForType) {
+            System.out.println(s);
+        }
+        DBHelper bean1 = run.getBean(DBHelper.class);
+        System.out.println(bean1);
+
     }
 
 }
---
+++ src/main/java/com/mei/config/MyConfig.java
@@ -0,0 +1,42 @@
+package com.mei.config;
+
+import ch.qos.logback.core.db.DBHelper;
+import com.mei.beans.Pet;
+import com.mei.beans.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+
+/**
+ * 1.配置类里面使用@Bean标注在方法上给容器注册组件，默认也是单实例的
+ * 2.配置类本身也是组件
+ * 3.proxyBeanMethods：代理bean的方法
+ *    Full(proxyBeanMethods = true)
+ *    Lite(proxyBeanMethods = false)
+ *    组件依赖必须使用Full模式默认。其他默认是否Lite模式
+ * 4.@Import({User.class, DBHelper.class})
+ *   给容器中自动创建出这两个类型的组件,默认组件的名字就是全类名
+ */
+
+@Import({User.class, DBHelper.class})
+@Configuration(proxyBeanMethods = true) //告诉springboot这是一个配置类 == 以前的配置文件
+public class MyConfig {
+    /**
+     * 外部无论对配置类中的这个组件注册方法调用多少次，获取到的都是之前注册容器中的单实例对象
+     * @return
+     */
+    @Bean //给容器中添加组件。以方法名作为组件的id。返回类型就是组件类型。返回的值就是组件在容器中的实例
+    public User user01(){
+        User zhangsan = new User("张三", 18);
+        //user组件依赖了pet组件
+        zhangsan.setPet(this.tomcat());
+        return zhangsan;
+    }
+
+    @Bean("tom")
+    public Pet tomcat(){
+        return new Pet("tomcat");
+    }
+
+}
